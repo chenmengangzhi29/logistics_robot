dc09d22be92d06993f80e42cf0e6e902d35756ca
XiaoHuang <q935325906@gmail.com>
2026-06-07 00:44:24 +0800

    feat(control): PickAndPlace action server的pick阶段加gripper HAL

--- robot_bringup/launch/pick_place_server.launch.py
+++ robot_bringup/launch/pick_place_server.launch.py
@@ -4,32 +4,31 @@ from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from moveit_configs_utils import MoveItConfigsBuilder
 
-
 def generate_launch_description():
     use_sim_time = LaunchConfiguration("use_sim_time")
 
     moveit_config = (
-    MoveItConfigsBuilder(
-        "warehouse_robot", 
-        package_name="ur5_warehouse_moveit_config"
+        MoveItConfigsBuilder(
+            "warehouse_robot",
+            package_name="ur5_warehouse_moveit_config"
         )
         .to_moveit_configs()
     )
 
-    move_group_node = Node(
-        package="moveit_ros_move_group",
-        executable="move_group",
+    pick_place_server = Node(
+        package="robot_control",
+        executable="pick_place_action_server",
         output="screen",
         parameters=[
             moveit_config.to_dict(),
             {"use_sim_time": use_sim_time},
         ],
     )
-    
+
     return LaunchDescription([
         DeclareLaunchArgument(
             "use_sim_time",
             default_value="false",
         ),
-        move_group_node,
+        pick_place_server,
     ])
